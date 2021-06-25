e1e549c7b9a790381675b7cce5dbf28415fca3e0
wdg <wdg2571363943@outlook.com>
2021-06-25 13:55:30 +0800

    注释语句

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ require('./app/database');
 
 const config = require('./app/config');
 
+
 app.listen(config.APP_PORT, () => {
-  console.log(`服务器在${config.APP_PORT}端口启动成功~`);
+    console.log(`服务器在${config.APP_PORT}端口启动成功~`);
 });
